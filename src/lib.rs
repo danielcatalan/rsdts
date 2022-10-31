b14747a34439775e2d9729ebdfaf2028e815a6d5
Daniel Catalan <dancatalan415@gmail.com>
2022-10-31 09:03:44 -0700

    correct syntax

--- src/lib.rs
+++ src/lib.rs
@@ -7,15 +7,9 @@ mod tests {
 
     #[test]
     fn basic_filter() {
-        // auto filter = sdts::Filter<2,1>::CreateFilter([n=0](const auto& x, auto& y)
-        // {
-        //     y[n] = x[n-1];
-        // });
-
-        let f = |x: i32, y: i32| x * y;
 
         let filter = Filter::<2, 1>::create_filter(|x, y| {
-            y[0] = x[x - 1];
+            y[0] = x[-1];
         });
 
         let x_in: [f64; 6] = [0.0, 1.0, 2.0, 3.0, 4.0, 5.0];
