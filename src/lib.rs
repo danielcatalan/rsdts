43ce186033516a835a314bba1c7de8ddc279e500
Daniel Catalan <dancatalan415@gmail.com>
2022-10-31 17:49:47 -0700

    add delay

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ mod tests {
     use crate::filter::Filter;
 
     #[test]
-    fn basic_filter() {
+    fn in_out() {
 
         let mut filter = Filter::<2, 2>::create_filter(|x, y| {
             y[0] = x[0];
@@ -23,5 +23,22 @@ mod tests {
         }
 
         assert_eq!(y_out, [1.0, 2.0, 3.0, 4.0, 5.0, 6.0])
+    }
+
+    #[test]
+    fn delay() {
+
+        let mut filter = Filter::<2, 2>::create_filter(|x, y| {
+            y[0] = x[-1];
+        });
+
+        let x_in: [f64; 6] = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0];
+        let mut y_out: [f64; 6] = [0.0; 6];
+
+        for i in 0..6 {
+            y_out[i] = filter.filt(x_in[i]);
+        }
+
+        assert_eq!(y_out, [0.0, 1.0, 2.0, 3.0, 4.0, 5.0])
     }
 }
