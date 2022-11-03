e2ff016ac8e2975dbbe508b6ab722559629211bf
Daniel Catalan <dancatalan415@gmail.com>
2022-11-02 23:40:20 -0700

    add foo

--- examples/exponential_smoothing.rs
+++ examples/exponential_smoothing.rs
@@ -1,13 +1,20 @@
 
-use simple_discrete_time_system::filter::Filter;
+use simple_discrete_time_system::filter::{Filter,DiffEq};
 
+
+fn foo() -> Box<dyn DiffEq>{
+    let alpha = 0.1;
+
+    let filter1 = Box::new(Filter::<1,2>::create_filter(move |x,y|{
+        y[0] = alpha * x[0] + (1.0-alpha) * y[-1];
+    }));
+    return filter1;
+}
 
 fn main(){
-    let alpha = 0.1;
+
     {
-        let mut filter1 = Filter::<1,2>::create_filter(|x,y|{
-            y[0] = alpha * x[0] + (1.0-alpha) * y[-1];
-        });
+        let mut filter1 = foo(); 
 
         for _ in 0..10{
             let y = filter1.filt(1.0);
