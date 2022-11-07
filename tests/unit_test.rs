2a5d450305f5d2dc612b703003469549a42c938e
Daniel Catalan <dancatalan415@gmail.com>
2022-11-06 19:35:12 -0800

    add unit test

---
+++ tests/unit_test.rs
@@ -0,0 +1,18 @@
+use sdts::filter::FilterCreator;
+use sdts::filter::Filter;
+
+#[test]
+pub fn filter1() {
+    let mut filt1 = FilterCreator::<1,1>::create_filter(|x,y|{
+        y[0] = x[0];
+    });
+
+    let xin = [1.0,2.0,3.0,4.0,5.0];
+    let mut yout = [0.0,0.0,0.0,0.0,0.0];
+
+    for i in 0..5{
+        yout[i] = filt1.filt(xin[i]);
+    }
+
+    assert_eq!(yout, xin);
+}
