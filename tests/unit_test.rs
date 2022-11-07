8f5a0ebb5fbe59ecffae602d0e4936a35c2e171d
Daniel Catalan <dancatalan415@gmail.com>
2022-11-06 19:39:08 -0800

    add delay

--- tests/unit_test.rs
+++ tests/unit_test.rs
@@ -16,3 +16,20 @@ pub fn straight_through() {
 
     assert_eq!(yout, xin);
 }
+
+#[test]
+pub fn delay() {
+    let mut filt1 = FilterCreator::<2,1>::create_filter(|x,y|{
+        y[0] = x[-1];
+    });
+
+    let xin = [1.0,2.0,3.0,4.0,5.0];
+    let expected = [0.0,1.0,2.0,3.0,4.0];
+    let mut yout = [0.0,0.0,0.0,0.0,0.0];
+
+    for i in 0..5{
+        yout[i] = filt1.filt(xin[i]);
+    }
+
+    assert_eq!(yout, expected);
+}
