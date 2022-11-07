de323830a1adb4ff1ff0699b43f15cf95de03540
Daniel Catalan <dancatalan415@gmail.com>
2022-11-06 19:10:21 -0800

    change test

--- examples/exponential_smoothing.rs
+++ examples/exponential_smoothing.rs
@@ -3,7 +3,7 @@ use sdts::filter::{FilterCreator, Filter};
 
 
 fn foo() -> Box<dyn Filter>{
-    let alpha = 0.1;
+    let alpha = 0.9;
 
     let filter1 = Box::new(FilterCreator::<1,2>::create_filter(move |x,y|{
         y[0] = alpha * x[0] + (1.0-alpha) * y[-1];
@@ -15,10 +15,10 @@ fn main(){
 
     let mut filter1 = foo(); 
 
-    for _ in 0..10{
+    for _ in 0..6{
         let y = filter1.filt(1.0);
 
-        print!("{y:.3}, ");
+        print!("{y}, ");
     }
     println!();
 }
