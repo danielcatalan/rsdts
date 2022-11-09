0f30af11b731ead135fd6c38c715a5166630218b
Daniel Catalan <dancatalan415@gmail.com>
2022-11-08 20:29:55 -0800

    cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -2,45 +2,6 @@ mod filter;
 mod signal;
 mod input_signal;
 mod output_signals;
+mod unit_test;
 
 pub use filter::{FilterCreator,Filter};
-
-#[cfg(test)]
-mod tests {
-
-    use crate::{FilterCreator,create_filter, Filter};
-
-    #[test]
-    fn in_out() {
-
-        let mut filter = create_filter!(1,1,|x, y| {
-            y[0] = x[0];}
-        );
-
-        let x_in: [f64; 6] = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0];
-        let mut y_out: [f64; 6] = [0.0; 6];
-
-        for i in 0..6 {
-            y_out[i] = filter.filt(x_in[i]);
-        }
-
-        assert_eq!(y_out, [1.0, 2.0, 3.0, 4.0, 5.0, 6.0])
-    }
-
-    #[test]
-    fn delay() {
-
-        let mut filter = create_filter!(2,2,|x, y| {
-            y[0] = x[-1];
-        });
-
-        let x_in: [f64; 6] = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0];
-        let mut y_out: [f64; 6] = [0.0; 6];
-
-        for i in 0..6 {
-            y_out[i] = filter.filt(x_in[i]);
-        }
-
-        assert_eq!(y_out, [0.0, 1.0, 2.0, 3.0, 4.0, 5.0])
-    }
-}
---
+++ src/unit_test.rs
@@ -0,0 +1,39 @@
+#[cfg(test)]
+mod integration_test{
+    use crate::{FilterCreator,Filter,create_filter};
+
+    #[test]
+    fn straight_through() {
+
+        let mut filter = create_filter!(1,1,|x, y| {
+            y[0] = x[0];
+        });
+
+        let x_in: [f64; 6] = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0];
+        let mut y_out: [f64; 6] = [0.0; 6];
+
+        for i in 0..6 {
+            y_out[i] = filter.filt(x_in[i]);
+        }
+
+        assert_eq!(y_out, [1.0, 2.0, 3.0, 4.0, 5.0, 6.0])
+    }
+
+
+    #[test]
+    pub fn delay() {
+        let mut filt1 = create_filter!(2,1,|x,y|{
+            y[0] = x[-1];
+        });
+
+        let xin = [1.0,2.0,3.0,4.0,5.0];
+        let expected = [0.0,1.0,2.0,3.0,4.0];
+        let mut yout = [0.0,0.0,0.0,0.0,0.0];
+
+        for i in 0..5{
+            yout[i] = filt1.filt(xin[i]);
+        }
+
+        assert_eq!(yout, expected);
+    }
+}
