f3f1bad38e599620440aa3ae2d8034c9acf6cdda
Daniel Catalan <dancatalan415@gmail.com>
2022-11-10 22:39:03 -0800

    add function for creation

--- examples/exponential_smoothing.rs
+++ examples/exponential_smoothing.rs
@@ -1,19 +1,15 @@
 
 use sdts::{Filter,create_filter,FilterCreator};
 
-fn foo() -> Box<dyn Filter>{
-    let alpha = 0.9;
-
-    let filter1 = Box::new(create_filter!(1,2, move |x,y| {
-        y[0] = alpha * x[0] + (1.0-alpha) * y[-1];
-    }));
-
-    return filter1;
+fn exp_smooth(a:f64) -> Box<dyn Filter>{
+    Box::new(create_filter!(1,2, move |x,y| {
+        y[0] = a * x[0] + (1.0-a) * y[-1];
+    }))
 }
 
 fn main(){
-
-    let mut filter1 = foo(); 
+    
+    let mut filter1 = exp_smooth(0.5); 
 
     for _ in 0..6{
         let y = filter1.filt(1.0);
