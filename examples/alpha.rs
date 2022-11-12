2042a727bd222bb6ab3c85acd1d8105743dbe593
Daniel Catalan <dancatalan415@gmail.com>
2022-11-11 19:03:14 -0800

    add parameter to test

--- examples/alpha.rs
+++ examples/alpha.rs
@@ -2,11 +2,11 @@ use sdts::{create_filter, Filter, FilterCreator};
 
 fn main() {
     let alpha = 0.9;
-    let mut filter1 = create_filter!(1, 2, move |x, y| {
+    let mut filter1 = create_filter!(f32, 1, 2, move |x, y| {
         y[0] = alpha * x[0] + (1.0 - alpha) * y[-1];
     });
 
-    for _ in 0..6 {
+    for _ in 0..9 {
         let y = filter1.filt(1.0);
 
         print!("{y}, ");
