a70e196979bcb63902d014abd4c7982ac16f5440
Daniel Catalan <dancatalan415@gmail.com>
2022-11-11 17:18:28 -0800

    rewrite example

--- examples/alpha.rs
+++ examples/alpha.rs
@@ -1,17 +1,12 @@
+use sdts::{create_filter, Filter, FilterCreator};
 
-use sdts::{Filter,create_filter, FilterCreator};
+fn main() {
+    let alpha = 0.9;
+    let mut filter1 = create_filter!(1, 2, move |x, y| {
+        y[0] = alpha * x[0] + (1.0 - alpha) * y[-1];
+    });
 
-fn exp_smooth(a:f64) -> impl Filter<f64>{
-    create_filter!(1,2, move |x,y| {
-        y[0] = a * x[0] + (1.0-a) * y[-1];
-    })
-}
-
-fn main(){
-    
-    let mut filter1 = exp_smooth(0.5); 
-
-    for _ in 0..6{
+    for _ in 0..6 {
         let y = filter1.filt(1.0);
 
         print!("{y}, ");
