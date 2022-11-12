74effcdd9e180c9f16768ea0860920d4c6dac9dd
Daniel Catalan <dancatalan415@gmail.com>
2022-11-11 16:24:27 -0800

    refactor

--- src/filter.rs
+++ src/filter.rs
@@ -1,6 +1,6 @@
 use std::marker::PhantomData;
 
-use crate::input_signal::XSeries;
+use crate::input_signal::InputSignal;
 use crate::output_signals::YSeries;
 
 pub trait Filter<NumType> {
@@ -10,21 +10,21 @@ pub trait Filter<NumType> {
 
 pub struct DifferenceEquation<NumType,const X: usize, const Y: usize, F>
 where
-    F: Fn(&XSeries<NumType,X>, &mut YSeries<NumType,Y>),
+    F: Fn(&InputSignal<NumType,X>, &mut YSeries<NumType,Y>),
 {
     functor: F,
-    xin: XSeries<NumType,X>,
+    xin: InputSignal<NumType,X>,
     yout: YSeries<NumType,Y>,
 }
 
 impl<NumType:Default+ std::marker::Copy,const X: usize, const Y: usize, F> DifferenceEquation<NumType,X, Y, F>
 where
-    F: Fn(&XSeries<NumType,X>, &mut YSeries<NumType,Y>),
+    F: Fn(&InputSignal<NumType,X>, &mut YSeries<NumType,Y>),
 {
     fn new(function: F) -> Self {
         DifferenceEquation {
             functor: function,
-            xin: XSeries::new(),
+            xin: InputSignal::new(),
             yout: YSeries::new(),
         }
     }
@@ -32,7 +32,7 @@ where
 
 impl<NumType:Default+ std::marker::Copy,const X: usize, const Y: usize, F> Filter<NumType> for DifferenceEquation<NumType,X, Y, F>
 where
-    F: Fn(&XSeries<NumType,X>, &mut YSeries<NumType,Y>),
+    F: Fn(&InputSignal<NumType,X>, &mut YSeries<NumType,Y>),
 {
     fn filt(&mut self, x: NumType) -> NumType {
         self.xin.push(x);
@@ -54,7 +54,7 @@ where
 {
     pub fn create_filter<F>(function: F) -> DifferenceEquation<NumType,X, Y, F>
     where
-        F: Fn(&XSeries<NumType,X>, &mut YSeries<NumType,Y>),
+        F: Fn(&InputSignal<NumType,X>, &mut YSeries<NumType,Y>),
     {
         DifferenceEquation::new(function)
     }
