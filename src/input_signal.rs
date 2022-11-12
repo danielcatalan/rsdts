89ef353f65dcb7a6fe3390c82eafdd408f6dda95
Daniel Catalan <dancatalan415@gmail.com>
2022-11-11 16:22:57 -0800

    refactor

--- src/input_signal.rs
+++ src/input_signal.rs
@@ -1,16 +1,16 @@
 use crate::signal::Signal;
 use std::ops::Index;
 
-pub struct XSeries<NumType, const XSIZE: usize> {
+pub struct InputSignal<NumType, const XSIZE: usize> {
     signal: Signal<NumType, XSIZE>,
 }
 
-impl<NumType, const XSIZE: usize> XSeries<NumType, XSIZE>
+impl<NumType, const XSIZE: usize> InputSignal<NumType, XSIZE>
 where
     NumType: Default + Copy,
 {
     pub fn new() -> Self {
-        XSeries {
+        InputSignal {
             signal: Signal::new(),
         }
     }
@@ -20,7 +20,7 @@ where
     }
 }
 
-impl<NumType, const XSIZE: usize> Index<i32> for XSeries<NumType, XSIZE>
+impl<NumType, const XSIZE: usize> Index<i32> for InputSignal<NumType, XSIZE>
 where
     NumType: Default + Copy,
 {
@@ -32,34 +32,34 @@ where
 
 #[cfg(test)]
 mod tests {
-    use crate::input_signal::XSeries;
+    use crate::input_signal::InputSignal;
 
     #[test]
     fn xseries() {
-        let mut x_series = XSeries::<f64, 3>::new();
+        let mut x_signal = InputSignal::<f64, 3>::new();
 
-        assert_eq!(x_series[0], 0.0);
-        assert_eq!(x_series[-1], 0.0);
-        assert_eq!(x_series[-2], 0.0);
+        assert_eq!(x_signal[0], 0.0);
+        assert_eq!(x_signal[-1], 0.0);
+        assert_eq!(x_signal[-2], 0.0);
 
-        x_series.push(1.0);
-        assert_eq!(x_series[0], 1.0);
-        assert_eq!(x_series[-1], 0.0);
-        assert_eq!(x_series[-2], 0.0);
+        x_signal.push(1.0);
+        assert_eq!(x_signal[0], 1.0);
+        assert_eq!(x_signal[-1], 0.0);
+        assert_eq!(x_signal[-2], 0.0);
 
-        x_series.push(2.0);
-        assert_eq!(x_series[0], 2.0);
-        assert_eq!(x_series[-1], 1.0);
-        assert_eq!(x_series[-2], 0.0);
+        x_signal.push(2.0);
+        assert_eq!(x_signal[0], 2.0);
+        assert_eq!(x_signal[-1], 1.0);
+        assert_eq!(x_signal[-2], 0.0);
 
-        x_series.push(3.0);
-        assert_eq!(x_series[0], 3.0);
-        assert_eq!(x_series[-1], 2.0);
-        assert_eq!(x_series[-2], 1.0);
+        x_signal.push(3.0);
+        assert_eq!(x_signal[0], 3.0);
+        assert_eq!(x_signal[-1], 2.0);
+        assert_eq!(x_signal[-2], 1.0);
 
-        x_series.push(4.0);
-        assert_eq!(x_series[0], 4.0);
-        assert_eq!(x_series[-1], 3.0);
-        assert_eq!(x_series[-2], 2.0);
+        x_signal.push(4.0);
+        assert_eq!(x_signal[0], 4.0);
+        assert_eq!(x_signal[-1], 3.0);
+        assert_eq!(x_signal[-2], 2.0);
     }
 }
