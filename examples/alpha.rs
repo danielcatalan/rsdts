021e1644077082b2b5769d6f3726ecbe87902a03
Daniel Catalan <dancatalan415@gmail.com>
2022-11-16 20:52:49 -0800

    edit test

--- examples/alpha.rs
+++ examples/alpha.rs
@@ -6,10 +6,13 @@ fn main() {
         y[0] = alpha * x[0] + (1.0 - alpha) * y[-1];
     });
 
-    for _ in 0..9 {
-        let y = filter1.process(1.0);
+    let x = [1.0;9];
+    let mut y = [0.0; 9];
+    
+    for i in 0..9 {
+        y[i] = filter1.process(x[i]);
 
-        print!("{y}, ");
+        print!("{}, ", y[i]);
     }
     println!();
 }
